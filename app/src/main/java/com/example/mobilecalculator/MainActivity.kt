25f245122621e96bb426fb0b5835eb1f6c734bff
Yauheni Shymanovich <17m2000y@gmail.com>
2021-01-26 03:32:14 +0300

    Initial commit. Project setup

---
+++ app/src/main/java/com/example/mobilecalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mobilecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun appendExpression(exp: String) {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/mobilecalculator/model/Calculator.kt
@@ -0,0 +1,5 @@
+package com.example.mobilecalculator.model
+
+class Calculator {
+
+}
